5cb9692fdb46cf108d6048a07f738773f219bb5c
NikolaTunguz <56549419+NikolaTunguz@users.noreply.github.com>
2023-12-14 16:01:19 -0800

    Updated state changes with water sensor

--- FinalProject.ino
+++ FinalProject.ino
@@ -14,6 +14,12 @@ volatile unsigned char *myUCSR0C  = (unsigned char *)0x00C2;
 volatile unsigned int  *myUBRR0   = (unsigned int *) 0x00C4;
 volatile unsigned char *myUDR0    = (unsigned char *)0x00C6;
 
+// ADC Pointers
+volatile unsigned char *my_ADMUX = (unsigned char*) 0x7C;
+volatile unsigned char *my_ADCSRB = (unsigned char*) 0x7B;
+volatile unsigned char *my_ADCSRA = (unsigned char*) 0x7A;
+volatile unsigned int  *my_ADC_DATA = (unsigned int*) 0x78;
+
 // GPIO Pointers
 volatile unsigned char *portE = 0x2E; //Start/Stop Button
 volatile unsigned char *portDDRE = 0x2D; 
@@ -36,10 +42,14 @@ LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
 //global variables
 bool disabled = true;
 bool error, idle, running;
+unsigned int waterThreshold = 25;
+unsigned int waterLevel;
 
 void setup() {
   U0Init(9600);
+  //Serial.begin(9600);
   setup_timer_regs();
+  adc_init();
   lcd.begin(16, 2);
 
   //set LEDs to output and LOW (PF0-3)
@@ -52,6 +62,7 @@ void setup() {
 }
 
 void loop() {
+
   if(disabled){
     disableState();
   }
@@ -81,14 +92,24 @@ void errorState(){
   lcd.write("Water level is");
   lcd.setCursor(0, 1);
   lcd.write("too low.");
+  
+  waterLevel = adc_read(5);
+  if(waterLevel > waterThreshold){
+    idle = true;
+    error = false;
+    disabled = false; 
+    running = false;
+  }
 }
 void idleState(){
   lightGreen();
   writeData();
+  checkWater();
 }
 void runningState(){
   lightBlue();
   writeData();
+  checkWater();
 }
 
 //LED light up functions
@@ -118,7 +139,15 @@ void writeData(){
   lcd.setCursor(0, 1);
   lcd.write("Humidity:");
 }
-
+void checkWater(){
+  waterLevel = adc_read(5);
+  if(waterLevel < waterThreshold){
+    idle = false;
+    error = true;
+    disabled = false; 
+    running = false;
+  }
+}
 // Timer setup function
 void setup_timer_regs(){
   // setup the timer control registers
@@ -147,7 +176,7 @@ ISR(TIMER1_OVF_vect)
   if(currentTicks != 65535)
   {
     // XOR to toggle PB6
-    *portB ^= 0x40;
+    //*portB ^= 0x40;
   }
 }
 
@@ -185,4 +214,44 @@ unsigned char getChar(){
 void putChar(unsigned char U0pdata){
   while((*myUCSR0A & TBE)==0);
   *myUDR0 = U0pdata;
+}
+//ADC functions
+void adc_init()
+{
+  // setup the A register
+  *my_ADCSRA |= 0b10000000; // set bit   7 to 1 to enable the ADC
+  *my_ADCSRA &= 0b11011111; // clear bit 6 to 0 to disable the ADC trigger mode
+  *my_ADCSRA &= 0b11110111; // clear bit 5 to 0 to disable the ADC interrupt
+  *my_ADCSRA &= 0b11111000; // clear bit 0-2 to 0 to set prescaler selection to slow reading
+  // setup the B register
+  *my_ADCSRB &= 0b11110111; // clear bit 3 to 0 to reset the channel and gain bits
+  *my_ADCSRB &= 0b11111000; // clear bit 2-0 to 0 to set free running mode
+  // setup the MUX Register
+  *my_ADMUX  &= 0b01111111; // clear bit 7 to 0 for AVCC analog reference
+  *my_ADMUX  |= 0b01000000; // set bit   6 to 1 for AVCC analog reference
+  *my_ADMUX  &= 0b11011111; // clear bit 5 to 0 for right adjust result
+  *my_ADMUX  &= 0b11100000; // clear bit 4-0 to 0 to reset the channel and gain bits
+}
+unsigned int adc_read(unsigned char adc_channel_num)
+{
+  // clear the channel selection bits (MUX 4:0)
+  *my_ADMUX  &= 0b11100000;
+  // clear the channel selection bits (MUX 5)
+  *my_ADCSRB &= 0b11110111;
+  // set the channel number
+  if(adc_channel_num > 7)
+  {
+    // set the channel selection bits, but remove the most significant bit (bit 3)
+    adc_channel_num -= 8;
+    // set MUX bit 5
+    *my_ADCSRB |= 0b00001000;
+  }
+  // set the channel selection bits
+  *my_ADMUX  += adc_channel_num;
+  // set bit 6 of ADCSRA to 1 to start a conversion
+  *my_ADCSRA |= 0x40;
+  // wait for the conversion to complete
+  while((*my_ADCSRA & 0x40) != 0);
+  // return the result in the ADC data register
+  return *my_ADC_DATA;
 }
